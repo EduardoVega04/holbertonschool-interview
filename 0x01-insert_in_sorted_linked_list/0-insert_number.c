8cfbefa9bf1495d409abbf1ed6a3f1b05131c7e7
EduardoVega04 <eduarvega04@hotmail.com>
2020-12-15 10:07:23 -0500

    Insert a node in a singly linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,37 +11,41 @@ listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *aux_fast = NULL, *aux_slow = NULL, *new_node = NULL;
 
-    /* If head is NULL */
-    if(head == NULL || *head == NULL)    
-        return NULL;
-    
-    /* Dynamically create the new node */
+    if(head == NULL)
+        return (NULL);
+
     new_node = malloc(sizeof(listint_t));
+    if(new_node == NULL)
+	    return (NULL);
+
     new_node->n = number;
     new_node->next = NULL;
 
-    /* First case: Number to insert is <= head */
-    if((*head)->n >= number)
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    if ((*head)->n > number)
     {
         new_node->next = *head;
         *head = new_node;
-        return new_node;
+        return (new_node);
     }
 
-    /* Second case: Number to insert > head and < than last number in list */  
     aux_slow = *head;
-    for(aux_fast = (*head)->next; aux_fast != NULL; aux_fast = aux_fast->next)
+    for (aux_fast = (*head)->next; aux_fast != NULL; aux_fast = aux_fast->next)
     {
-        if(aux_fast->n > number)
+        if (aux_fast->n > number)
         {
             aux_slow->next = new_node;
-            new_node->next = aux_fast;            
+            new_node->next = aux_fast;
             return new_node;
         }
         aux_slow = aux_fast;
     }
 
-    /* Third case: Number to insert is >= last number */
     new_node = add_nodeint_end(head, (const int)number);
-    return new_node; 
+    return (new_node);
 }
