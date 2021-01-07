64f74969ade38ff0d8b32cc67fc696e73961c0e2
EduardoVega04 <eduarvega04@hotmail.com>
2021-01-07 16:03:12 -0500

    Minimum operations to get n number of 'H' characters

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -5,5 +5,5 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 6
+n = 5
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,23 +7,16 @@ In the text file, there is initially a single character 'H'
 """
 
 
-def prime_or_not(n):
-    """Determine if n is prime or not
-    - Divide n from n / 2 to 2
-    - If there is a divisor, return False (n is not prime)
-    - If there is not a divisor, return True (n is prime)"""
-    for i in range(n // 2, 1, -1):
-        if n % i == 0:
-            return False
-    return True
-
-
 def minOperations(n):
-    """The main function. Returns integer"""
-    if type(n) != int or n <= 1:
+    if type(n) != int or n < 0:
         return 0
-    for i in range(n // 2, 1, -1):  # We need the largest prime divisor
+
+    res = 0
+    i = 2
+    while i <= n:
         if n % i == 0:
-            if prime_or_not(i) is True:
-                return i + (n // i)
-    return n
+            res += i
+            n = n / i
+            i = i - 1
+        i = i + 1
+    return int(res)
