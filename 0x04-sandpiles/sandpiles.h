dd64d43d4cc3701d56a346be634ae7cc8d9de585
EduardoVega04 <eduarvega04@hotmail.com>
2021-01-14 20:17:19 -0500

    Fixed header

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,6 +1,9 @@
 #ifndef _sandpile_h__
 #define _sandpile_h__
 
+#include <stdio.h>
+#include <stdlib.h>
+
 void myprint_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void topple(int grid1[3][3]);
