c9124ef5de0ff782a344a13bd2c62e166b0d119a
EduardoVega04 <eduarvega04@hotmail.com>
2021-08-18 04:35:00 -0500

    Rain

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,15 +6,17 @@ def rain(walls):
     """Return: Integer indicating total amount of rainwater retained"""
     totalWater = leftWall = distance = 0
 
-    if walls and isinstance(walls, list):
-        for wall in walls:
-            if wall != 0 and leftWall == 0:
-                leftWall = wall
-            elif wall == 0 and leftWall != 0:
-                distance += 1
-            else:
-                totalWater += distance * min(leftWall, wall)
-                leftWall = wall
-                distance = 0
+    if not walls or not isinstance(walls, list):
+        return 0
+
+    for wall in walls:
+        if wall != 0 and leftWall == 0:
+            leftWall = wall
+        elif wall == 0 and leftWall != 0:
+            distance += 1
+        else:
+            totalWater += distance * min(leftWall, wall)
+            leftWall = wall
+            distance = 0
 
     return totalWater
