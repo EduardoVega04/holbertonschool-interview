6ac2fc8415d03eca7c1dd8abf5f6893e99845bdb
EduardoVega04 <eduarvega04@hotmail.com>
2021-12-15 14:15:19 -0500

    Fixed style

--- 0x20-regex/regex.h
+++ 0x20-regex/regex.h
@@ -1,8 +1,10 @@
-#ifndef REGEX_H
-#define REGEX_H
+#ifndef _REGEX_H_
+#define _REGEX_H_
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int regex_match(char const *str, char const *pattern);
 
-#endif /* REGEX_H */
+#endif /* _REGEX_H_ */
